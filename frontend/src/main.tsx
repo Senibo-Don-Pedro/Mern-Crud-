b9950cb1a187a25d232287f9c2517e43ed3af467
Senibo-Don-Pedro <senibodonpedro@gmail.com>
2024-03-13 17:44:14 +0100

    prod errors

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -4,6 +4,11 @@ import App from './App.tsx'
 import './index.css'
 import { AuthContextProvider } from './context/AuthContext.tsx'
 import { WorkoutsContextProvider } from './context/WorkoutContext'
+import { disableReactDevTools } from '@fvilers/disable-react-devtools'
+
+if (process.env.NODE_ENV === 'production') {
+  disableReactDevTools()
+}
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
